ed644ed6accb848d61af59f38ae4ae1e4f37acfb
Ruila <4momolmr@gmail.com>
2023-04-08 00:33:07 +0800

    feat: test path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hero-skills/",
+  base: "./hero-skills/",
   plugins: [react()],
 })
